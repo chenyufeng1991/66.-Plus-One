660cd531fa8bd0b8b8fbc7184a90908447920852
陈宇峰 <yufengcode@gmail.com>
2016-10-04 17:54:43 +0800

    1

--- 66. Plus One/main.cpp
+++ 66. Plus One/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits)
+{
+    for (long i = digits.size() - 1; i >= 0; i--)
+    {
+        if (digits[i] + 1 == 10)
+        {
+            digits[i] = 0;
+            if (i == 0)
+            {
+                // 头部插入1；
+                digits.insert(digits.begin(), 1);
+            }
+            continue;
+        }
+        else
+        {
+            digits[i] += 1;
+            break;
+        }
+    }
+
+    return digits;
+}
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {9,9,9};
+    vector<int>vv(arr,arr+sizeof(arr)/sizeof(int));
+    vector<int> result;
+    result = plusOne(vv);
+
     return 0;
 }
+
+
+
+
